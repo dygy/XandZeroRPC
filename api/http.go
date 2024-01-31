7367838d14863532965430d68c0acfb8b23e22f3
Arkadii Shwartsman <a.shvartcman@gmail.com>
2024-01-31 22:08:14 +0100

    DYGYRA-13: #comment Vercel trying more

--- api/http.go
+++ api/http.go
@@ -88,12 +88,3 @@ func parsePlace(w http.ResponseWriter, req *Request, xorzero *Table) {
 	xorzero.checkWinner()
 	GETTable(w, xorzero)
 }
-
-func Cors(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=ascii")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,access-control-allow-origin, access-control-allow-headers, request, action, id")
-	var req = parseHeader(r)
-
-	rpc(w, req, xorzero)
-}
--- api/index.go
+++ api/index.go
@@ -6,9 +6,18 @@ import (
 
 var xorzero = new(Table)
 
-func Main() {
+func main() {
 	xorzero.init()
 	mux := http.NewServeMux()
 	mux.HandleFunc("/api/", Cors)
 	http.ListenAndServe(":3333", mux)
+}
+
+func Cors(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=ascii")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,access-control-allow-origin, access-control-allow-headers, request, action, id")
+	var req = parseHeader(r)
+
+	rpc(w, req, xorzero)
 }
