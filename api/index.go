20aa8e6f019c4e9124e9659a9768308accd85431
Arkadii Shwartsman <a.shvartcman@gmail.com>
2024-01-31 22:48:01 +0100

    DYGYRA-13: #comment Vercel trying more

--- api/index.go
+++ api/index.go
@@ -6,14 +6,8 @@ import (
 
 var xorzero = new(Table)
 
-func main() {
-	xorzero.init()
-	mux := http.NewServeMux()
-	mux.HandleFunc("/api/", Cors)
-	http.ListenAndServe(":3333", mux)
-}
-
 func Cors(w http.ResponseWriter, r *http.Request) {
+	xorzero.init()
 	w.Header().Set("Content-Type", "text/html; charset=ascii")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,access-control-allow-origin, access-control-allow-headers, request, action, id")
